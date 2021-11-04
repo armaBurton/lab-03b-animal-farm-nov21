a49dc5a2feb66775cad81725db554f2fdfaf209a
armaBurton <armanarma@armaburton.com>
2021-11-04 16:04:21 -0700

    refactoring

--- app.js
+++ app.js
@@ -17,82 +17,83 @@ const vasquez = document.getElementById('vasquez');
 const clickedCharacter = document.getElementById('clicked-character');
 
 aliens.addEventListener('click', () => {
-    let audio = new Audio('./assets/aliens/screech01.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Xenomorph';
+    playSound('./assets/aliens/screech03.mp3', ':: Xenomorph');
 });
 
 apone.addEventListener('click', () => {
-    let audio = new Audio('./assets/apone/assholes-elbows.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Sergeant Al Apone';
+    playSound('./assets/apone/assholes-elbows.mp3', ':: Sergeant Al Apone');
 });
 
 bishop.addEventListener('click', () => {
-    let audio = new Audio('./assets/bishop/synthetic.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Lance Bishop 341-B synthetic humanoid';
+    playSound('./assets/bishop/synthetic.mp3', ':: Lance Bishop 341-B synthetic humanoid');
 });
 
 burke.addEventListener('click', () => {
-    let audio = new Audio('./assets/burke/grunt.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Carter J. Burke';
+    playSound('./assets/burke/grunt.mp3', ':: Carter J. Burke');
 });
 
 ferro.addEventListener('click', () => {
-    let audio = new Audio('./assets/ferro/pipe.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Corporal Colette Ferro';
+    playSound('./assets/farro/pipe.mp3', ':: Corporal Colette Ferro');
 });
 
 frost.addEventListener('click', () => {
-    let audio = new Audio('./assets/frost/bad-feeling.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Private First Class Ricco Frost';
+    playSound('./assets/frost/bad-feeling.mp3', ':: Private First Class Ricco Frost');
 });
 
 hicks.addEventListener('click', () => {
-    let audio = new Audio('./assets/hicks/purpose.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Corporal Dwayne Hicks';
+    playSound('./assets/hicks/purpose.mp3', ':: Corporal Dwayne Hicks');
 });
 
 hudson.addEventListener('click', () => {
-    let audio = new Audio('./assets/hudson/demoralized.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Private First Class William L. Hudson';
+    playSound('./assets/hudson/demoralized.mp3', ':: Private First Class William L. Hudson');
 });
 
 techno.addEventListener('click', () => {
-    let audio = new Audio('./assets/misc/fifteen-minutes-safe-distance.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Emergency Broadcast System';
+    playSound('./assets/misc/fifteen-minutes-safe-distance.mp3', ':: Emergency Broadcast System');
 });
 
 newt.addEventListener('click', () => {
-    let audio = new Audio('./assets/newt/shortcut.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Rebecca "Newt" Jorden';
+    playSound('./assets/newt/shortcut.mp3', ':: Rebecca "Newt" Jorden');
 });
 
 ripley.addEventListener('click', () => {
-    let audio = new Audio('./assets/ripley/iqs.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Lieutenant Ellen Ripley';
+    playSound('./assets/ripley/iqs.mp3', ':: Lieutenant Ellen Ripley');
 });
 
 vasquez.addEventListener('click', () => {
-    let audio = new Audio('./assets/vasquez/pendejo.mp3');
-    audio.play();
-    clickedCharacter.textContent = ':: Private First Class Jenette Vasquez';
+    playSound('./assets/vasquez/pendejo.mp3', ':: Private First Class Jenette Vasquez');
 });
 
+function playSound(sound, name){
+    let audio = new Audio(sound);
+    audio.play();
+    clickedCharacter.textContent = name;
+}
 
-// reference needed DOM elements
-
-
-// set event listeners 
-    // get user input(s)
-    // do any needed work with the value(s)
-    // update DOM to reflect new value(s)
+document.addEventListener('keydown', (e) => {
+    if (e.key === '1') {
+        playSound('./assets/aliens/screech03.mp3', ':: Xenomorph');
+    } else if (e.key === '2') {
+        playSound('./assets/apone/assholes-elbows.mp3', ':: Sergeant Al Apone');
+    } else if (e.key === '3') {
+        playSound('./assets/bishop/synthetic.mp3', ':: Lance Bishop 341-B synthetic humanoid');
+    } else if (e.key === '4') {
+        playSound('./assets/burke/grunt.mp3', ':: Carter J. Burke');
+    } else if (e.key === '5') {
+        playSound('./assets/farro/pipe.mp3', ':: Corporal Colette Ferro');
+    } else if (e.key === '6') {
+        playSound('./assets/frost/bad-feeling.mp3', ':: Private First Class Ricco Frost');
+    } else if (e.key === '7') {
+        playSound('./assets/hicks/purpose.mp3', ':: Corporal Dwayne Hicks');
+    } else if (e.key === '8') {
+        playSound('./assets/hudson/demoralized.mp3', ':: Private First Class William L. Hudson');
+    } else if (e.key === '9') {
+        playSound('./assets/misc/fifteen-minutes-safe-distance.mp3', ':: Emergency Broadcast System');
+    } else if (e.key === '0') {
+        playSound('./assets/newt/shortcut.mp3', ':: Rebecca "Newt" Jorden');
+    } else if (e.key === '-') {
+        playSound('./assets/ripley/iqs.mp3', ':: Lieutenant Ellen Ripley');
+    } else if (e.key === '=') {
+        playSound('./assets/vasquez/pendejo.mp3', ':: Private First Class Jenette Vasquez');
+    }
+})
